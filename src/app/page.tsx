f317b2026c1da6642caa478d4824678039c36c5d
SimonPringleWallace <s.pringlewallace@gmail.com>
2024-12-12 20:10:41 -0500

    Add User Search - Input Debounced

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { Input } from "@/components/ui/input";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
+import { useDebouncedCallback } from "use-debounce";
 
 const formSchema = z.object({
   name: z.string().nonempty({
@@ -24,6 +25,17 @@ export default function Home() {
     console.log(data);
   };
 
+  const searchForUsers = useDebouncedCallback(
+    async (e: React.ChangeEvent<HTMLInputElement>) => {
+      console.log(e.target.value);
+      const res = await fetch(`https://api.github.com/search/users?q=${e.target.value}`, {
+        headers: {
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_GITHUB_API_KEY}`,
+        },
+      })
+      console.log(await res.json());
+  }, 500)
+
   return (
     <div className="mt-20">
       <Form {...form}>
@@ -31,11 +43,15 @@ export default function Home() {
           <FormField
             name="name"
             control={form.control}
-            render={({ field }) => (
+            render={({field: { onChange, ...field }}) => (
               <FormItem>
                 <FormLabel>User name</FormLabel>
                 <FormControl>
                   <Input
+                    onChange={(e) => {
+                      searchForUsers(e);
+                      onChange(e);
+                    }}
                     className="w-60"
                     placeholder="GitHub Username"
                     {...field}
